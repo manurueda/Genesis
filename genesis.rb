a1949f97936cb50a4979920739bbd15c4bab1676
Manu Rueda <jm.glezderueda@gmail.com>
2024-08-31 16:23:34 +0200

    Bump version to v0.1.2

--- genesis.rb
+++ genesis.rb
@@ -3,8 +3,8 @@ class Genesis < Formula
   
   desc "Project Genesis: The Dawn of Autonomous Code Evolution"
   homepage "https://github.com/manurueda/Genesis"
-  url "https://github.com/manurueda/Genesis/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://github.com/manurueda/Genesis/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "662d76c1ea1efb346b0f1540d4ab0703bed77fe46e364800505042afd8de731d"
   license "MIT"
 
   depends_on "python@3.12"
