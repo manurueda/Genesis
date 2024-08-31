8d5b373f08cf3bf96e2e9a0fd7188e7fc6a633a3
Manu Rueda <jm.glezderueda@gmail.com>
2024-08-31 16:22:29 +0200

    Bump version to v0.1.2

--- genesis.rb
+++ genesis.rb
@@ -4,33 +4,15 @@ class Genesis < Formula
   desc "Project Genesis: The Dawn of Autonomous Code Evolution"
   homepage "https://github.com/manurueda/Genesis"
   url "https://github.com/manurueda/Genesis/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "662d76c1ea1efb346b0f1540d4ab0703bed77fe46e364800505042afd8de731d"
   license "MIT"
 
   depends_on "python@3.12"
 
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
-    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
-  end
-
-  resource "langchain" do
-    url "https://files.pythonhosted.org/packages/..."
-    sha256 "..."
-  end
-
-  resource "pytest" do
-    url "https://files.pythonhosted.org/packages/..."
-    sha256 "..."
-  end
-
-  resource "requests" do
-    url "https://files.pythonhosted.org/packages/..."
-    sha256 "..."
-  end
-
   def install
     virtualenv_install_with_resources
+    system "poetry", "export", "-f", "requirements.txt", "--output", "requirements.txt", "--without-hashes"
+    virtualenv.pip_install "requirements.txt"
   end
 
   test do
