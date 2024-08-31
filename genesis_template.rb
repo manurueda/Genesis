30b1d2d05b4bd05380c2bc5cc2beab803313918d
Manu Rueda <jm.glezderueda@gmail.com>
2024-08-31 17:07:49 +0200

    Update genesis_template.rb

--- genesis_template.rb
+++ genesis_template.rb
@@ -9,26 +9,6 @@ class Genesis < Formula
 
   depends_on "python@3.12"
 
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
-    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
-  end
-
-  resource "langchain" do
-    url "https://files.pythonhosted.org/packages/..."
-    sha256 "..."
-  end
-
-  resource "pytest" do
-    url "https://files.pythonhosted.org/packages/..."
-    sha256 "..."
-  end
-
-  resource "requests" do
-    url "https://files.pythonhosted.org/packages/..."
-    sha256 "..."
-  end
-
   def install
     virtualenv_install_with_resources
   end
